ebdce8c6028d7e293f1a702b4105d84899b2c51a
juany360 <juaninv@outlook.com>
2016-04-25 17:40:59 -0300

    ejericico 8, todavia no anda

--- simusched/sched_rr2.cpp
+++ simusched/sched_rr2.cpp
@@ -7,32 +7,96 @@
 using namespace std;
 
 SchedRR2::SchedRR2(vector<int> argn) {
-	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro	
+	for (int i = 0; i < argn[0]; ++i){
+		queue<int> cola;		
+		q.push_back(cola);
+	}
+
+	for (int i = 0; i < argn[0]; ++i){
+		vector<int> tareas;
+		tasks.push_back(tareas);
+	}
+
+	cpu_quantum.clear();
+	def_quantum.clear();
+	
+	for (int i = 1; i <= argn[0]; ++i){		
+		cpu_quantum.push_back(argn[i]);
+		def_quantum.push_back(argn[i]);
+	}
 }
 
 SchedRR2::~SchedRR2() {
-
 }
 
 
-void SchedRR2::load(int pid) {
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+void SchedRR2::load(int pid) {	
+	int menor = q[0].size() ;	
+	for (int i = 0; i < q.size(); ++i) {
+		if(q[i].size()<menor){
+			menor = i;
+		}
+	}		
+	q[menor].push(pid);	
+	tasks[menor].push_back(pid);		
 }
 
-void SchedRR2::unblock(int pid) {
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+void SchedRR2::unblock(int pid) {	
+	int nucleo = -1;
+	for (int i = 0; i < tasks.size(); ++i) {
+		for (int j = 0; j < tasks[i].size(); ++j) {
+			if(tasks[i][j] == pid);
+			nucleo = i;
+		}
+	}
+	q[nucleo].push(pid);
 }
 
 int SchedRR2::tick(int cpu, const enum Motivo m) {
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+int siguiente;	
+	if (current_pid(cpu) == IDLE_TASK){
+		siguiente = next(cpu);			
+		return siguiente;
+	} 
+	if (m == EXIT){
+		def_quantum[cpu] = cpu_quantum[cpu];
+		siguiente = next(cpu);		
+		for (int i = 0; i < tasks.size(); ++i) {
+			for (int j = 0; j < tasks[i].size(); ++j) {
+				if(tasks[i][j] == current_pid(cpu)){
+					tasks[i].erase(tasks[i].begin()+j);
+				}
+			}
+		}
+		return siguiente;
+	}
+	if (m == BLOCK){
+		def_quantum[cpu] = cpu_quantum[cpu];
+		siguiente = next(cpu);	
+		return siguiente;			
+	}
+	if (m == TICK){
+		def_quantum[cpu]--;
+		if (def_quantum[cpu] == 0){
+			def_quantum[cpu] = cpu_quantum[cpu];
+			q[cpu].push(current_pid(cpu));
+			siguiente = next(cpu);			
+		} else {
+			siguiente = current_pid(cpu);
+		}
+		return siguiente;		
+	}	
+	
 }
 
 int SchedRR2::next(int cpu) {
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+	int next;
+	if (!q[cpu].empty()) {
+		next = q[cpu].front();
+		q[cpu].pop();
+	} else {
+		next = IDLE_TASK;
+	}
+	return next;	
 }
--- simusched/sched_rr2.h
+++ simusched/sched_rr2.h
@@ -8,20 +8,18 @@
 class SchedRR2 : public SchedBase {
 	public:
 		SchedRR2(std::vector<int> argn);
-        ~SchedRR2();
+       		 ~SchedRR2();
 		virtual void load(int pid);
 		virtual void unblock(int pid);
 		virtual int tick(int cpu, const enum Motivo m);
 
 	private:
-//<<<REMOVE>>>
-		std::vector< std::queue<int> *> q;
-		std::vector< std::vector<int> *> tasks;
+		std::vector< std::queue<int> > q;
+		std::vector< std::vector<int> > tasks;
 		std::vector<int> cpu_quantum;
 		std::vector<int> def_quantum;
-		int	lastcpu;
+		int lastcpu;
 		int ncores;
-//<<<REMOVE END>>>
 		int next(int cpu);
 };
 
